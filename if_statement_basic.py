a723358d6ab0b67a89e9ffe80d13f60713d004fb
shenarie nicole <MagsayoShenarieNicole_Bsit@plmun.edu.ph>
2025-02-15 03:00:06 +0800

    Submitting Python Week 04 - Laboratory # 08

--- if_statement_basic.py
+++ if_statement_basic.py
@@ -4,8 +4,18 @@
 # Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
 
 def main():
-   temperature = 35  # Or any other number
-if temperature > 35:
+
+
+temperature = 35  # Or any other number
+
+# Write an if statement
+if temperature > 30:
+    # Print a message (indented)
     print("It's a hot day!")  # Indented 4 spaces
+
+# Note: There is no else block, so if temperature is 30 or less, there will be no output
+
+if __name__ == "__main__":
+    main()
 
             
